88aaf51756cafd56479ff44d7558fefc397baca4
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2024-06-09 10:37:50 +0600

    1st part of the project

--- 08. Advanced Node.js & Express Bangla by Foyzul---/Project/index.js
+++ 08. Advanced Node.js & Express Bangla by Foyzul---/Project/index.js
@@ -1,11 +1,50 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import models from './models/index.js';
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+const log = (msg) => console.log(msg);
+
+const uri = 'mongodb://localhost:27017/parcelkoi';
+const options = {};
+
+const connectWithDb = () => {
+  mongoose
+    .connect(uri, options)
+    .then(() => {
+      console.log('Database connection established');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+connectWithDb();
 
 app.get('/', (req, res) => {
-  res.send('Alhumdulillahsssddsssss  f');
+  res.send('Alhumdulillah inshaAllah ' + req.query.id);
+});
+
+app.post('/', (req, res) => {
+  const body = req.body;
+  const user = new models.User({
+    username: body.username,
+    createdAt: new Date(),
+  });
+  user
+    .save()
+    .then((saveUser) => {
+      res.status(201).send('User save ID: ' + saveUser._id);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
 });
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
+
+log(models);
--- 08. Advanced Node.js & Express Bangla by Foyzul---/Project/models/index.js
+++ 08. Advanced Node.js & Express Bangla by Foyzul---/Project/models/index.js
@@ -1,4 +1,4 @@
-import User from './user';
+import User from './users.js';
 
 const models = { User };
 
